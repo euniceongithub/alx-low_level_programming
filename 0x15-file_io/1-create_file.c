1bbf1f85b3b9ba7fa33b22e9953f2437b15e2242
euniceakligo <eyakligo@st.ug.edu.gh>
2024-09-16 14:10:49 +0000

    -

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	length = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
@@ -40,5 +40,5 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
-	return (-1);
+	return (1);
 }
